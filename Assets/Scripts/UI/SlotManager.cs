7b058556026bfc80848bb15d8cbc005b875348ef
DESKTOP-INTKMK1\young <kke7078@naver.com>
2025-08-11 17:33:41 +0900

    슬롯매니저 스크립트, 슬롯 프리팹(추후 수정 필요) 추가

---
+++ Assets/Scripts/UI/SlotManager.cs
@@ -0,0 +1,40 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace mp
+{
+    public class SlotManager : MonoBehaviour    //머지판 위에 어떤 아이템이 어디에 놓여있는 지 관리
+    {
+        [SerializeField] private GameObject slotPrefab;
+        private RectTransform rectTransform;
+
+        private int slotColCount = 7;
+        private int slotRowCount = 10;
+        private int slotSpacing = 5;
+        private int SlotTotalCount => slotColCount * slotRowCount;
+
+        private void Start()
+        {
+            rectTransform = GetComponent<RectTransform>();
+
+            StartCoroutine("SlotSetting");
+        }
+
+        IEnumerator SlotSetting()
+        {
+            yield return null;
+
+            // slot 크기 설정
+            int slotWidth = ((int)rectTransform.rect.width - (slotSpacing * (slotColCount - 1)) - 20) / slotColCount;
+            int slotHeight = ((int)rectTransform.rect.height - (slotSpacing * (slotRowCount - 1)) - 20) / slotRowCount;
+
+            //slot 복제
+            for (int i = 0; i < SlotTotalCount + 7; i++) 
+            { 
+                GameObject slot = Instantiate(slotPrefab, rectTransform);
+            };
+        }
+    }
+}
