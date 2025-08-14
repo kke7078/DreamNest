07605beb537adc2820a57ad45cbb48ded18933b5
DESKTOP-INTKMK1\young <kke7078@naver.com>
2025-08-14 14:48:23 +0900

    product Item (생설될 아이템) 카테고리 별 데이터, product Item 데이터 베이스 에셋 추가 (스크립터블 오브젝터로 관리)

---
+++ Assets/Scripts/Data/ProductItemDataBase.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace mp
+{
+    [CreateAssetMenu(menuName = "Data/ProductItemDataBase")]
+    public class ProductItemDataBase : ScriptableObject //프로덕트 아이템 데이터베이스 클래스
+    {
+        public List<ProductItemList> allItemList = new List<ProductItemList>();
+
+        public List<ProductItemData> GetItemsByCategory(ItemCategory category)
+        {
+            ProductItemList list = allItemList.Find(itemList => itemList.Category == category);
+            if (list != null) return list.items;
+            
+            return new List<ProductItemData>();
+        }
+    }
+}
---
+++ Assets/Scripts/Data/ProductItemList.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace mp
+{
+    public enum ItemCategory //아이템의 종류
+    {
+        Dog,
+        Cat,
+        Rabbit
+    }
+
+    [System.Serializable]
+    public class ProductItemData
+    {
+        public Sprite icon;
+        public string itemName;
+        [TextArea] public string description;
+    }
+
+    [CreateAssetMenu(menuName = "Data/ProductItemList")]
+    public class ProductItemList : ScriptableObject //아이템의 정보를 담고 있는 클래스
+    {
+        [SerializeField] private ItemCategory category;
+        public ItemCategory Category => category; //아이템의 종류를 반환하는 프로퍼티
+
+        public List<ProductItemData> items = new List<ProductItemData>(); //아이템 리스트
+    }
+}
