2c8c8e7c93be17d472e1040aa86994a8c2d1824c
DESKTOP-8I1H4BM\young <kke7078@naver.com>
2025-09-16 10:59:12 +0900

    블록아이템데이터 + 블록아이템리스트 파일 분리

---
+++ Assets/Scripts/Data/Item/BlockItemData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DreamNest
+{
+    
+    [System.Serializable]
+    public class BlockItemData : BaseItemData
+    {
+        [SerializeField] ItemPrice itemSellPrice;
+        [SerializeField] ItemPrice itemBuyPrice;
+        public ItemPrice ItemSellPrice => itemSellPrice;
+        public ItemPrice ItemBuyPrice => itemBuyPrice;
+    }
+}
---
+++ Assets/Scripts/Data/Item/BlockItemList.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DreamNest
+{
+    [CreateAssetMenu(menuName = "Data/BlockItemList")]
+    public class BlockItemList : BaseItemList
+    {
+        [SerializeField] private ItemElementType itemElementType;
+        [SerializeField] private List<BlockItemData> itemDataList;
+
+        public ItemElementType ItemElementType  => itemElementType;
+        public List<BlockItemData> ItemDataList => itemDataList;
+
+
+        //리스트 아이템 정보 입력
+        public void OnEnable()
+        {
+            if(itemDataList == null) itemDataList = new List<BlockItemData>();
+            SetItemInfo(itemDataList);
+        }
+    }
+}
