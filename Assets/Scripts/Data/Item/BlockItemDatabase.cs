2e81eb255cabde31874bc4e126556a96c046cef2
DESKTOP-8I1H4BM\young <kke7078@naver.com>
2025-10-13 14:02:38 +0900

    블록/생성기 아이템 데이터베이스 코드 추가

---
+++ Assets/Scripts/Data/Item/BlockItemDatabase.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using Unity.VisualScripting.ReorderableList;
+using UnityEngine;
+using static UnityEditor.Progress;
+
+namespace DreamNest
+{
+    [CreateAssetMenu(menuName = "Data/BlockItemDB")]
+    public class BlockItemDatabase : ScriptableObject
+    {
+        [SerializeField] private List<BlockItemList> blockItemList;
+        public List<BlockItemList> BlockItemList => blockItemList;
+
+        private Dictionary<string, BaseItemData> itemDict;
+
+        public void BuildDictionary()
+        {
+            if (itemDict != null) return;
+            
+            itemDict = new Dictionary<string, BaseItemData>();
+
+            foreach (var list in blockItemList) //블록아이템들의 리스트 순회
+            {
+                foreach (var item in list.ItemDataList)
+                {
+                    BaseItemData baseData = item as BaseItemData;
+                    if (baseData != null)
+                    {
+                        if (!itemDict.ContainsKey(baseData.ItemID))
+                        {
+                            itemDict.Add(baseData.ItemID, baseData);
+                        }
+                    }
+                }
+            }
+        }
+
+        public BaseItemData GetItemById(string id)
+        {
+            if (itemDict == null) BuildDictionary();
+            itemDict.TryGetValue(id, out var item);
+
+            return item;
+        }
+
+        public BlockItemData GetRandomItem(GeneratorData generator)
+        {
+            
+
+            return null;
+        }
+
+        //임시 가중치 함수
+        private int GetWeight(ItemGrade grade)
+        {
+            switch (grade)
+            {
+                case ItemGrade.Normal: return 100;
+                case ItemGrade.Rare: return 50;
+                default: return 0;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Data/Item/GeneratorItemDatabase.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DreamNest
+{
+    [CreateAssetMenu(menuName = "Data/GeneratorItemDB")]
+    public class GeneratorItemDatabase : ScriptableObject
+    {
+        private static GeneratorItemDatabase instance;
+        public static GeneratorItemDatabase Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = Resources.Load<GeneratorItemDatabase>("GeneratorItemDB");
+                }
+
+                return instance;
+            }
+        }
+
+        [SerializeField] private List<GeneratorItemList> generatorItemList;
+        public List<GeneratorItemList> GeneratorItemLists => generatorItemList;
+
+        private Dictionary<string, BaseItemData> itemDict;
+
+        public void BuildDictionary()
+        {
+            if (itemDict != null) return;
+
+            itemDict = new Dictionary<string, BaseItemData>();
+
+            foreach (var list in generatorItemList) //생성기 아이템들의 리스트 순회
+            {
+                foreach (var item in list.ItemDataList)
+                {
+                    BaseItemData baseData = item as BaseItemData;
+                    if (baseData != null)
+                    {
+                        if (!itemDict.ContainsKey(baseData.ItemID))
+                        {
+                            itemDict.Add(baseData.ItemID, baseData);
+                        }
+                    }
+                }
+            }
+        }
+
+        public BaseItemData GetItemById(string id)
+        {
+            if (itemDict == null) BuildDictionary();
+            itemDict.TryGetValue(id, out var item);
+
+            Debug.Log(item);
+
+            return item;
+        }
+    }
+}
