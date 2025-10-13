5d81ebd0f95e1d7e05330446dcbd4add7509050a
DESKTOP-8I1H4BM\young <kke7078@naver.com>
2025-10-13 14:08:13 +0900

    GamaManager 싱글톤 추가

---
+++ Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace DreamNest
+{
+    public class GameManager : MonoBehaviour
+    {
+        public static GameManager Instance { get; private set; }
+
+        [SerializeField] private BlockItemDatabase blockItemDB;
+        [SerializeField] private GeneratorItemDatabase generatorItemDB;
+
+        public BlockItemDatabase BlockItemDB => blockItemDB;
+
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+            else Destroy(gameObject);
+        }
+
+        private void Start()
+        {
+            blockItemDB.BuildDictionary();
+
+            foreach (var generatorItemList in generatorItemDB.GeneratorItemLists)
+            {
+                generatorItemList.InitializeSpawnList();
+            }
+        }
+    }
+}
